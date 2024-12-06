079cede59c0714794f14178fd97d8f30c4ec4876
Jabin Tasmin Anny <jabintasmin102523@gmail.com>
2024-12-06 20:43:10 +0600

    Exam on python Basic

---
+++ main.py
@@ -0,0 +1,9 @@
+# main.py
+
+# Import the custom module
+import myModule
+
+# Use the module's functions and variables
+print(myModule.greet("Anny"))
+print("Sum:", myModule.add_numbers(5, 7))
+print("Value of Pi:", myModule.pi)
---
+++ myModule.py
@@ -0,0 +1,9 @@
+# myModule.py
+
+def greet(name):
+    return f"Hello, {name}!"
+
+def add_numbers(a, b):
+    return a + b
+
+pi = 3.14159
